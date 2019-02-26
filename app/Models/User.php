d197795ffd84bea99c20805127d742b6bc78d843
Jun.Lee <jun.lee@qq.com>
2019-02-27 04:47:17 +0800

    用户微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,7 +45,7 @@ class User extends Authenticatable
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
-    public funciton statuses()
+    public function statuses()
     {
         return $this->hasMany(Status::class);
     }
